801179d8f4fc6f91d0b5cddbd7c5c2539803f233
Yoonsangbyung <ysb1706@naver.com>
2024-04-05 15:27:56 +0900

    컴퓨터프로그래밍5주차

---
+++ quiz_5.py
@@ -0,0 +1,15 @@
+a = input("점수를 입력하세요 : ")
+num_a = int(a)
+
+if num_a  >= 71 :
+    print("A")
+
+elif num_a >= 41 :
+    print("B")
+
+elif num_a >= 11 :
+    print("C")
+
+else :
+    print("D")
+
---
+++ test5.py
@@ -0,0 +1,11 @@
+age = input("나이를 입력하세요 :")
+num_age = int(age)
+
+if num_age > 19 :
+    print("성인입니다.")
+
+elif num_age < 19 :
+    print("미성년자입니다.")
+
+
+
